07628181f545910dc9885ee068bdba3c3288c95f
Keidson Roby <keidsondesigner@gmail.com>
2023-10-28 16:19:37 -0400

    Passando eventos entre pai e filho

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -5,13 +5,32 @@
       <img src="@/assets/ranek.svg" alt="">
     </router-link>
     <router-link to="/login" class="btn">Login</router-link>
+    <TesteEventEmit @enviandoClick="recebendoClick" />
+    <!-- <p v-if="dadosRecebidosDoEmit">{{ dadosRecebidosDoEmit }}</p> -->
+    <p v-if="dadosRecebidosDoEmit.nome">{{ dadosRecebidosDoEmit }}</p>
   </nav>
   </header>
 </template>
 
 <script>
+import TesteEventEmit from '@/components/TesteEventEmit.vue';
+
 export default {
   name: 'HeaderComponent',
+  components: {
+    TesteEventEmit,
+  },
+  data() {
+    return {
+      dadosRecebidosDoEmit: Object,
+    };
+  },
+  methods: {
+    recebendoClick(dadosRecebidos) {
+      console.log('recebendoClick');
+      this.dadosRecebidosDoEmit = dadosRecebidos;
+    },
+  },
 };
 </script>
 
---
+++ src/components/TesteEventEmit.vue
@@ -0,0 +1,43 @@
+<template>
+  <button @click="enviandoClick">Teste EventEmit</button>
+</template>
+
+<script>
+export default {
+  name: 'TesteEventEmit',
+  data() {
+    return {
+      isPwd: true,
+      isLogin: false,
+      isPrimeiroAcesso: true,
+      isDadosprofissionais: false,
+      isDadosAcesso: false,
+      newCadastro: {
+        nome: undefined,
+        nascimento: undefined,
+        sexo: undefined,
+        cpf: undefined,
+        email: undefined,
+        telefone: undefined,
+      },
+    };
+  },
+  methods: {
+    enviandoClick() {
+      console.log('emitindo evento');
+      this.newCadastro = {
+        nome: 'keidson',
+        nascimento: '21/08/1990',
+        sexo: 'masculino',
+        cpf: '99208237249',
+        email: 'keidson@gmail.com',
+        telefone: '92994571858',
+      };
+      this.$emit('enviandoClick', { ...this.newCadastro });
+      console.log('emitindo evento e enviando dados', this.newCadastro);
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
