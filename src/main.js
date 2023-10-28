a2c9f44397b6da4d44adfcd1ec58cf755b1d8399
Keidson Roby <keidsondesigner@gmail.com>
2023-10-28 18:48:20 -0400

    Pipe filter de formatacao de moeda

--- src/main.js
+++ src/main.js
@@ -7,6 +7,14 @@ import store from './store';
 const app = createApp(App);
 
 app.component('LoadingComponent', LoadingComponent);
+app.config.globalProperties.$filters = {
+  formatarMoeda(value) {
+    return new Intl.NumberFormat('pt-BR', {
+      style: 'currency',
+      currency: 'BRL',
+    }).format(value);
+  },
+};
 app.use(store);
 app.use(router);
 app.mount('#app');
