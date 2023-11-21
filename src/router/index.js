53fe6eac4df1ffcf3d1da7c47448c471ec52513b
Keidson Roby <keidsondesigner@gmail.com>
2023-11-21 14:42:45 -0400

    atualização de rotas

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,10 @@ import HomeView from '../views/HomeView.vue';
 import LoginView from '../views/LoginView.vue';
 import ProdutoDetalhesView from '../views/ProdutoDetalhesView.vue';
 import UsuarioView from '../views/usuario/UsuarioView.vue';
+import UsuarioProdutoView from '../views/usuario/UsuarioProdutoView.vue';
+import UsuarioEditarView from '../views/usuario/UsuarioEditarView.vue';
+import UsuarioComprasView from '../views/usuario/UsuarioComprasView.vue';
+import UsuarioVendasView from '../views/usuario/UsuarioVendasView.vue';
 
 const routes = [
   {
@@ -23,8 +27,29 @@ const routes = [
   },
   {
     path: '/usuario',
-    name: 'usuario',
     component: UsuarioView,
+    children: [
+      {
+        path: '',
+        name: 'usuario-produtos',
+        component: UsuarioProdutoView,
+      },
+      {
+        path: 'editar',
+        name: 'usuario-editar',
+        component: UsuarioEditarView,
+      },
+      {
+        path: 'compras',
+        name: 'compras',
+        component: UsuarioComprasView,
+      },
+      {
+        path: 'vendas',
+        name: 'vendas',
+        component: UsuarioVendasView,
+      },
+    ],
   },
 ];
 
